2445d82fc1b6b3e50aa0b2c09887e5ee77ce1ac8
Michael Zhao <michaeloz@10-18-76-134.dynapool.wireless.nyu.edu>
2019-06-04 12:54:12 -0400

    hw3

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -4,33 +4,56 @@
 using namespace std;
 
 void inc_by_value(int n) {
+    n++;
 }
 
 
 void inc_with_pointer(int* iptr) {
+    (*iptr)++;
 }
 
 
 void inc_with_reference(int& n) {
+    n++;
 }
 
 
 Thing** create_array_of_things(int n) {
-    return nullptr;
+    Thing** things = new Thing*[n];
+    for(int i = 0; i < n; ++i){
+        things[i] = new Thing(i);
+    }
+    return things;
 }
 
 void print_all_things(Thing** things, int n) {
+    for(int i = 0; i < n; ++i){
+        cout << things[i] -> val << " ";
+    }
+    cout << endl;
 }
 
 void double_all_things(Thing** things, int n) {
+    for(int i = 0; i < n; ++i){
+        things[i] -> val *= 2;
+    }
 }
 
 void delete_all_things(Thing** things, int n) {
+    for(int i = 0; i < n; ++i){
+        delete things[i];
+    }
+    delete[] things;
 }
 
 
-void assignTA(Student& s, Student& ta) {
+void assignTA(Student* s, Student* ta) {
+    s -> ta = ta;
 }
 
-void printTAs(vector<Student> students) {
+void printTAs(vector<Student*> students) {
+    for(int i = 0; i < students.size(); ++i){
+        cout << students[i] -> ta -> name << " ";
+    }
+    cout << endl; 
 }
