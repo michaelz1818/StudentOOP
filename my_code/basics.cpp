9de8dc355bb92614f061e91a386d2efbffc728f4
Michael Zhao <michaeloz@michaels-mbp-51.home>
2019-06-04 00:40:22 -0400

    testing new code

--- my_code/basics.cpp
+++ my_code/basics.cpp
@@ -9,12 +9,14 @@ using namespace std;
  * increment arg passed by copy
  * */
 void inc_pass_copy(int j) {
+    j++;
 }
 
 /*
  * increment arg passed by reference
  * */
 void inc_pass_ref(int& j) {
+    j++;
 }
 
 /*
@@ -24,12 +26,18 @@ void inc_pass_ref(int& j) {
  * Use `push_back()` to fill the vector.
  * */
 void fill_vector(vector<int>& v, int n) {
+    for(int i = 1; i < n+1; ++i){
+        v.push_back(i);
+    }
 }
 
 /*
  * Print all elements in a vector. Use a ranged for!
  * */
 void print_vector(const vector<int>& v) {
+    for(int i: v){
+        cout << i << endl;
+    }
 }
 
 /*
@@ -37,41 +45,55 @@ void print_vector(const vector<int>& v) {
  * Output should look like "x = 9, y = 2".
  * */
 void print_coords(coord pos) {
+    cout << "pos.x = " << pos.x << ", pos.y = " << pos.y << endl;
 }
 
 /*
  * Square an integer.
  * */
 int square(int n) {
-    return 100;
+    return n * n;
 }
 
 /*
  * Square a double.
  * */
 double square(double d) {
-    return 6.25;
+    return d * d;
 }
 
 /*
  * Get the absolute value of a number.
  * */
 int my_abs(int n) {
-    return 10;
+    if(n < 0){
+        return n * -1;
+    }
+    return n;
 }
 
 /*
  * Sum from 0 to n using a `while` loop.
  * */
 int sum(int n) {
-    return 45;
+    int i = 0;
+    int result = 0;
+    while (i < n){
+        result += i;
+        ++i;
+    }
+    return result;
 }
 
 /*
  * Sum from 0 to n using a `for` loop.
  * */
 int sum2(int n) {
-    return 45;
+    int result = 0
+    for(int i = 0; i < n+1; ++i){
+        result += i;
+    }
+    return result;
 }
 
 /*
@@ -86,4 +108,8 @@ void say(std::string s) {
  * Use a ranged for to pull a string apart into its bits.
  * */
 void pull_apart_string(std::string s) {
+    vector<char> vecString = {};
+    for(char a : s){
+        vecString.push_back(a);
+    }
 }
