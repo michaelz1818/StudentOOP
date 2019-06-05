64068fb4340142e1af483aa1f171f9fd94e75220
michaelz1818 <51217400+michaelz1818@users.noreply.github.com>
2019-06-05 11:25:32 -0400

    New test_pointer.cpp file for hw3

--- tests/test_pointers.cpp
+++ tests/test_pointers.cpp
@@ -20,7 +20,7 @@ int main() {
     inc_with_reference(n);
     cout << "After ref, n = " << n << endl;
     assert(n == 2);
-    
+
     // now assign TAs to students
     vector<Student*> students;
     Student* mary = new Student("mary");
@@ -29,19 +29,21 @@ int main() {
     students.push_back(mary);
     students.push_back(gordon);
     printTAs(students);
-    
+    assert(mary->ta == gordon);
+    assert(gordon->ta == nullptr);
+
     // now let's make, print, change, and delete some "things":
     Thing** things = create_array_of_things(NUM_THINGS);
-    // assert(things != nullptr);
-    
+    assert(things != nullptr);
+
     print_all_things(things, NUM_THINGS);
-    
+
     double_all_things(things, NUM_THINGS);
     for (int i = 0; i < NUM_THINGS; i++)
-        ; // assert(things[i]->val == (i * 2));
-    
+        assert(things[i]->val == (i * 2));
+
     print_all_things(things, NUM_THINGS);
-    
+
     delete_all_things(things, NUM_THINGS);
     return 0;
 }
