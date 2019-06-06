5bfdd9163eb6c0e2d20ea58111674bdc7bf12d99
Michael Zhao <michaeloz@10-18-76-134.dynapool.wireless.nyu.edu>
2019-06-06 12:13:47 -0400

    testing new code

---
+++ my_code/weather.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include "weather.h"
+
+using namespace std;
+
+/*
+ * A constructor for weather class.
+ * */
+Weather::Weather(std::string nm, GPS loc):
+station_nm(nm), my_loc(loc) {
+}
+
+ostream& operator << (ostream& os, const Weather& w){
+    os << "Name: " << w.station_nm << ", Location: " << w.my_loc << ", Rating: " << w.rating << endl;
+    return os;
+}
+
+int Weather::get_rating() const {
+    return rating;
+}
+
+void Weather::set_rating(int new_rating){
+    rating = new_rating; 
+}
+
+string Weather::get_name() const {
+    return station_nm;
+}
